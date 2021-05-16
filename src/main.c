f45321224515f7f1fd31c8832b61befba5126848
shimoncohen <33935191+shimoncohen@users.noreply.github.com>
2021-05-16 21:28:34 +0300

    chore(gpkg paths): verify gpkg paths exist (#6)

--- src/main.c
+++ src/main.c
@@ -4,12 +4,13 @@
 #include "threadPool/threadPool.h"
 #include "tileBatch.h"
 
-int count;
-
-void work(void *args)
+void doesPathExist(char *fullPath, char *file)
 {
-    count++;
-    printf("%d\n", count);
+    if (fullPath == NULL)
+    {
+        printf("No such file: %s\n", file);
+        exit(-1);
+    }
 }
 
 int main(int argc, char **argv)
@@ -20,6 +21,7 @@ int main(int argc, char **argv)
     if (argc != 4)
     {
         printf("Usage: %s <path_to_base_gpkg> <path_to_new_gpkg> <batch_size>\n", argv[0]);
+        printf("Example: %s area1.gpkg area2.gpkg 1000\n", argv[0]);
         exit(-1);
     }
 
@@ -29,6 +31,9 @@ int main(int argc, char **argv)
     char *baseGpkgPath = realpath(argv[1], NULL);
     char *newGpkgPath = realpath(argv[2], NULL);
     int batchSize = atoi(argv[3]);
+
+    doesPathExist(baseGpkgPath, argv[1]);
+    doesPathExist(newGpkgPath, argv[2]);
 
     // Read gpkgs
     Gpkg *baseGpkg = readGpkgInfo(baseGpkgPath);
