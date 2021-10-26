396e922f62b71c0355e2536e069f2435a16c1968
Shahar s <cl-shaharshavit-rnd@users.noreply.github.com>
2021-10-26 14:16:34 +0300

    chore: added missing changes to header

--- src/ImageWand/upscaling.h
+++ src/ImageWand/upscaling.h
@@ -3,7 +3,7 @@
 #include "wandUtil.h"
 #include "../tile.h"
 
-void lastTileSQLQuery(char *, char *);
-Tile *bindParametersToQueryAndExecute(sqlite3 *, char *, int[]);
+void lastTileSQLQuery(char *, char *, int);
+Tile *bindParametersToQueryAndExecute(sqlite3 *, char *, int[], int);
 Tile *getLastExistingTile(int, int, int, sqlite3 *, char *);
 void upscale(MagickWand *baseWand, Tile *baseTile, int newZoomLevel, int newX, int newY);
